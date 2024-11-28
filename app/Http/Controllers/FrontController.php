b055de8934a1a822cc7f3949f4ebd5bf2bf855d8
subhomoy samanta <subhomoy@keylines.net>
2024-11-28 13:35:36 +0530

    client feedback points given on 27-11-2024

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -33,15 +33,11 @@ class FrontController extends Controller
         $current_date       = date('Y-m-d');
         /* cron for admin */
             $notifications      = DB::table('consignments')
-                                                    // ->join('consignments', 'consignment_details.consignment_id', '=', 'consignments.id')
                                                     ->join('customers', 'consignments.customer_id', '=', 'customers.id')
                                                     ->join('pols', 'consignments.pol', '=', 'pols.id')
                                                     ->join('pods', 'consignments.pod', '=', 'pods.id')
-                                                    // ->join('process_flows', 'consignment_details.process_flow_id', '=', 'process_flows.id')
                                                     ->select('consignments.*', 'customers.name as customer_name', 'pols.name as pol_name', 'pods.name as pod_name')
                                                     ->where('consignments.status', '=', 1)
-                                                    // ->where('consignment_details.input_value', '=', '')
-                                                    // ->where('consignment_details.notification_date', '=', $current_date)
                                                     ->orderBy('consignments.id', 'DESC')
                                                     ->get();
             // Helper::pr($notifications);
@@ -54,7 +50,7 @@ class FrontController extends Controller
                     'notifications'     => $notifications,
                 ];
                 $message                     = view('email-templates.notification-template',$maildata);
-                echo $message;die;
+                // echo $message;die;
                 $subject                     = $generalSetting->site_name.' '.$mail_header;
                 $this->sendMail($generalSetting->system_email, $subject, $message);
             /* email sent */
@@ -78,17 +74,13 @@ class FrontController extends Controller
                                                     ->where('roles.import_access', '=', 1)
                                                     ->orderBy('admins.id', 'ASC')
                                                     ->get();
-            $notifications      = DB::table('consignment_details')
-                                                    ->join('consignments', 'consignment_details.consignment_id', '=', 'consignments.id')
+            $notifications      = DB::table('consignments')
                                                     ->join('customers', 'consignments.customer_id', '=', 'customers.id')
                                                     ->join('pols', 'consignments.pol', '=', 'pols.id')
                                                     ->join('pods', 'consignments.pod', '=', 'pods.id')
-                                                    ->join('process_flows', 'consignment_details.process_flow_id', '=', 'process_flows.id')
-                                                    ->select('consignments.*', 'customers.name as customer_name', 'pols.name as pol_name', 'pods.name as pod_name', 'process_flows.name as process_flow_name')
-                                                    ->where('consignment_details.status', '=', 1)
-                                                    // ->where('consignment_details.input_value', '=', '')
+                                                    ->select('consignments.*', 'customers.name as customer_name', 'pols.name as pol_name', 'pods.name as pod_name')
+                                                    ->where('consignments.status', '=', 1)
                                                     ->where('consignments.shipment_type', '=', 'Import')
-                                                    ->where('consignment_details.notification_date', '=', $current_date)
                                                     ->orderBy('consignments.id', 'DESC')
                                                     ->get();
             // Helper::pr($notifications);
@@ -129,17 +121,13 @@ class FrontController extends Controller
                                                     ->where('roles.export_access', '=', 1)
                                                     ->orderBy('admins.id', 'ASC')
                                                     ->get();
-            $notifications      = DB::table('consignment_details')
-                                                    ->join('consignments', 'consignment_details.consignment_id', '=', 'consignments.id')
+            $notifications      = DB::table('consignments')
                                                     ->join('customers', 'consignments.customer_id', '=', 'customers.id')
                                                     ->join('pols', 'consignments.pol', '=', 'pols.id')
                                                     ->join('pods', 'consignments.pod', '=', 'pods.id')
-                                                    ->join('process_flows', 'consignment_details.process_flow_id', '=', 'process_flows.id')
-                                                    ->select('consignments.*', 'customers.name as customer_name', 'pols.name as pol_name', 'pods.name as pod_name', 'process_flows.name as process_flow_name')
-                                                    ->where('consignment_details.status', '=', 1)
-                                                    // ->where('consignment_details.input_value', '=', '')
+                                                    ->select('consignments.*', 'customers.name as customer_name', 'pols.name as pol_name', 'pods.name as pod_name')
+                                                    ->where('consignments.status', '=', 1)
                                                     ->where('consignments.shipment_type', '=', 'Export')
-                                                    ->where('consignment_details.notification_date', '=', $current_date)
                                                     ->orderBy('consignments.id', 'DESC')
                                                     ->get();
             // Helper::pr($notifications);
