9c3bdf69d064a0c4dc68e9a802d16690dc0cd5b4
subhomoy samanta <subhomoy@keylines.net>
2024-12-17 15:32:16 +0530

    test email functionality implemented

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -54,7 +54,7 @@ class Controller extends BaseController
         if (!empty($file)):
             $mailLibrary->AddAttachment($file);
         endif;
-        Helper::pr($mailLibrary);
+        // Helper::pr($mailLibrary);
         return (!$mailLibrary->send()) ? false : true;
     }
     // single file upload
